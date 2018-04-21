dd7cff3382091f61ee094c9ae8e4651f76311d88
vagrant <1063720421@qq.com>
2018-04-21 05:08:14 +0000

    Topic order

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,10 +14,9 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request,Topic $topic)
 	{
-	    //方法 with() 提前加载了我们后面需要用到的关联属性 user 和 category，并做了缓存。数据已经被预加载并缓存，因此不会再产生多余的 SQL 查询：
-		$topics = Topic::with('user','category')->paginate(30);
+        $topics = $topic->withOrder($request->order)->paginate(20);
 		return view('topics.index', compact('topics'));
 	}
 
