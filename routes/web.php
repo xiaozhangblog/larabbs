945053be8a08ffcbca2d2a1a8165c845c4c4c026
vagrant <1063720421@qq.com>
2018-04-22 06:18:37 +0000

    User can upload topic image

--- routes/web.php
+++ routes/web.php
@@ -51,3 +51,7 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 
 // 帖子分类
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+// 图片上传
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
