6cb1119f966201d5d9e84b0d940923d7c3d044a1
vagrant <1063720421@qq.com>
2018-04-22 07:14:30 +0000

    Prevent XSS injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,6 +11,8 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+        // 对用户内容进行过滤。防跨站脚本攻击
+        $topic->body = clean($topic->body, 'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
