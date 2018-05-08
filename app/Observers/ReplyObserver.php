ad43a27d5890406fa6dc0e66d63e86617a230050
vagrant <1063720421@qq.com>
2018-05-08 02:50:56 +0000

    User can create reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -11,7 +11,11 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        //
+        //帖子回复数 + 1
+        $reply->topic->increment('reply_count',1);
+
+        // XSS 过滤
+        $reply->content = clean($reply->content,'user_topic_body');
     }
 
     public function updating(Reply $reply)
